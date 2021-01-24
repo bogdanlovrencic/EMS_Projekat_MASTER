8a0df884b47ef482159ad0330fd88a8bff01414a
Lesa96 <dragomirovic.sasa.96@gmail.com>
2021-01-24 16:20:50 +0100

    Added dbContext

---
+++ ModelLabs/NetworkModelService/NMSContext.cs
@@ -0,0 +1,19 @@
+﻿using FTN.Services.NetworkModelService.Model;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+
+namespace FTN.Services.NetworkModelService
+{
+    public class NMSContext : DbContext
+    {
+        DbSet<DeltaSave> deltaSaves { get; set; }
+
+        public NMSContext() : base("name=AppContextConnectionString")
+        {
+            //Database.SetInitializer<NMSContext>(new CreateDatabaseIfNotExists<NMSContext>());
+        }
+    }
+}
